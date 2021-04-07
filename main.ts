d3597f0d6bf6f7eaff69da535f9d3f1c5c6d8ac7
Justinbarry188 <79929356+Justinbarry188@users.noreply.github.com>
2021-04-07 10:53:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 let sub_1 = 0
 let Addition_1 = 0
+// these are one set of numbers 
 input.onPinPressed(TouchPin.P0, function () {
     sub_1 += 1
     basic.showNumber(sub_1)
 })
+// these are another section of numbers 
 input.onButtonPressed(Button.A, function () {
     Addition_1 += 1
     basic.showNumber(Addition_1)
 })
+// takes the two numbers a subtracts them 
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(Addition_1 - sub_1)
 })
@@ -19,6 +22,7 @@ input.onPinPressed(TouchPin.P1, function () {
     sub_1 += -1
     basic.showNumber(sub_1)
 })
+// takes the two numbers and adds them
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(Addition_1 + sub_1)
 })
